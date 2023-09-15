04017587f5b75a9ebb71a0c91011575047935552
Sisay Hiwot <angelhiwot@gmail.com>
2023-09-15 14:07:39 +0200

    end of home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,14 +9,23 @@ function Home(){
                <div className='your-fav'>
                 Your favorite Starwars <br/>
                 charactors
+                
                </div>
+               
                <div className='recently'>Recently viewed</div>
             
             </div> 
           <div className='photo-room'></div>
+          
           <div className='item-1'><div className='view'>View</div></div>
           <div className='item-2'><div className='view'>View</div></div>
           <div className='item-3'><div className='view'>View</div></div>
+          <div className='dots'>
+            <div className='dot-1'></div>
+            <div className='dot-2' ></div>
+            <div className='dot-3'></div>
+          </div>
+          
         </div>
     );
 
